c22f80cba57f8b95277d1725d9515865f54a1646
nevikw39 <nevikw39@icloud.com>
2021-09-01 12:08:58 +0800

    linear search

---
+++ 00_linear_search.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <climits>   // for INT_MAX
+#include <random>    // for mt19937
+#include <algorithm> // for find()
+
+using namespace std;
+
+int linear_search(vector<int>::iterator begin, vector<int>::iterator end, int target)
+{
+    int result = INT_MAX; // infinity
+    for (auto itr = begin; itr != end; itr++)
+        if (*itr >= target && *itr < result)
+            result = *itr;
+    return result;
+}
+
+int main()
+{
+    vector<int> v = {8, 27, 2021, 110, 20};
+    for (const int &i : v)
+        cout << i << ' ';
+    cout << "\nThe minimum element that greater than or equal to 110 is " << linear_search(v.begin(), v.end(), 110) << '\n';
+
+    int n;
+    cin >> n;
+    v.resize(n);
+    mt19937 _rand((random_device())()); // create an object with random seed that generates random numbers using Mersenne Twister Algo
+    for (int &i : v)
+    {
+        i = _rand();
+        cout << i << ' ';
+    }
+    cout << "\nThe minimum element that greater than or equal to 110 is " << linear_search(v.begin(), v.end(), 110) << '\n';
+    return 0;
+}
